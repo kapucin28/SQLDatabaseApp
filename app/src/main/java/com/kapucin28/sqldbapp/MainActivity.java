2b9978e8967039d5862ad8ce2e8e14a1f0cbb175
Timbuli Remus <kapucin28@gmail.com>
2016-05-19 13:07:48 +0300

    Creating menu method

--- app/src/main/java/com/kapucin28/sqldbapp/MainActivity.java
+++ app/src/main/java/com/kapucin28/sqldbapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.kapucin28.sqldbapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    //----------------------------------------------------------------------------------------------
+
+    // Creating menu method-------------------------------------------------------------------------
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
     //----------------------------------------------------------------------------------------------
 }
