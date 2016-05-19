01854ed63c33a9aec70f0a91751884a8c2e3f582
Timbuli Remus <kapucin28@gmail.com>
2016-05-19 13:15:12 +0300

    Creating menu items selection method

--- app/src/main/java/com/kapucin28/sqldbapp/MainActivity.java
+++ app/src/main/java/com/kapucin28/sqldbapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.kapucin28.sqldbapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +19,31 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    //----------------------------------------------------------------------------------------------
+
+    // Selected menu items actions method-----------------------------------------------------------
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case R.id.exit_app:
+                finish();
+                break;
+            case R.id.create_database:
+                break;
+            case R.id.delete_database:
+                break;
+            case R.id.display_DB:
+                break;
+            case R.id.clear_DB:
+                break;
+            case R.id.add_person:
+                break;
+            case R.id.remove_person:
+                break;
+        }
         return true;
     }
     //----------------------------------------------------------------------------------------------
